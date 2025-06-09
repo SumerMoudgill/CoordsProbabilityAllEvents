10c602ade31897b90cc3477cff807428c393136b
SumerMoudgill <52638124+SumerMoudgill@users.noreply.github.com>
2025-06-09 12:07:13 -0400

    Add files via upload

---
+++ CoordsProbabilitySmallPandas.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3 
+from pathlib import Path
+import numpy as np
+from matplotlib import pyplot as plt
+from hpmoc import PartialUniqSkymap
+from hpmoc.plot import get_wcs, plot, gridplot
+import sys
+from CoordsProbabilityCFolder.DetectorMinima import *
+from CoordsProbabilityCFolder.RankPoints import *
+from CoordsProbabilityCFolder.O4aSuperevents import *
+from CoordsProbabilityCFolder.CoordsProbabilityFunctions import *
+from CoordsProbabilityCFolder.SupereventRead import *
+from CoordsProbabilityCFolder.DetectorMinimaSkymap import *
+import pandas as pd
+
+#nesting:
+#superevent_to_probabilities
+#   which_detectors
+#   get_probabilities_at_minima
+#       get_probability_at_coords
+#           match_coords
+#           single_pixel_to_probability
+#superevent_array_map
+
+
+
+#event_dataset = ["event_name", "timeJulian", "detectors", "zero_locations", "detectors_triggered", "sigfigs", "values", "skymap_coord_string"]
+#for i in range(len(superevents)):
+#    current_superevent=superevents[i]
+#    event_name=current_superevent[0]
+#    timeJulian=current_superevent[2]
+#    detectors=which_detectors(event_array[3])
+#    current_superevent_array=superevents[i]
+#    print(superevent_to_probabilities(current_superevent_array, file_location="./testing/"))
+    #zero_locations from get_probabilities_at_minima, through superevent_to_probabilities
+    #detectors_triggered from get_probabilities_at_minima, through superevent_to_probabilities 
+    #sigfigs from math_coords, through get_probability_at_coords, get_probabilities_at_minima, and superevent_to_probabilities
+    #values from match_coords, through get_probability_at_coords, get_probabilities_at_minima, and superevent_to_probabilities
+    #skymap_coord_string from get_probability_at_coords, through get_probabilities_at_minima, and superevent_to_probabilities
+
+#print("hello world")
+#fits_a='./detected_fitsmaps/maps1_1.fits'
+#fits_b='./detected_fitsmaps/maps2_1.fits'
+#fits_c='./detected_fitsmaps/maps4_1.fits'
+#a = PartialUniqSkymap.read(fits_a, strategy='ligo')
+#b = PartialUniqSkymap.read(fits_b, strategy='ligo')
+#c = PartialUniqSkymap.read(fits_c, strategy='ligo')
+#print(type(a))
+#print(a)
+#print(a.coords())
+#print(type(a.plot()))
+#print(a.plot())
+
+#GW170817_array=["GW170817", 1, GW170817_JDT, "H1L1V1"]
+#print(superevent_to_probabilities(GW170817_array, file_location="./testing/"))
+#superevent_array_map(GW170817_array, "GW170817", file_location="./testing/")
+namescolumn=[]
+timescolumn = []
+detectorscolumn = []
+sf_column = []
+p_column = []
+f_column = []
+cf_column = []
+maxpoint_column = []
+max_sep_column = []
+for i in range(len(O4a_Sep_events_1)):
+    current_superevent_array=O4a_Sep_events_1[i]
+    current_superevent_read=superevent_read(current_superevent_array)
+    #print("Event name: ", current_superevent_array[0])
+    namescolumn.append(current_superevent_array[0])
+    #print("Event time: ", current_superevent_array[2])
+    timescolumn.append(current_superevent_array[2])
+    #print("Detectors list: ", which_detectors(current_superevent_array[3]))
+    detectorscolumn.append(which_detectors(current_superevent_array[3]))
+    n_detectors=len(which_detectors(current_superevent_array[3]))
+    res_p, res_sf, res_c, res_f=superevent_to_probabilities(current_superevent_array, file_location="./O4a_fits/")
+    #print("Probabilities list: ", res_p)
+    p_column.append(res_p)
+    #print("Significant figures list: ", res_sf)
+    sf_column.append(res_sf)
+    #print("Coordinates list: ", res_c)
+    #c_column.append(res_c)
+    #print("Flags list: ", res_f)
+    f_column.append(res_f)
+    cf_list=get_all_null_coords(current_superevent_array[2], which_detectors(current_superevent_array[3]))
+    res_maxpoint = max_point(current_superevent_read)
+    #print("Max point: ", res_maxpoint)
+    maxpoint_column.append(res_maxpoint)
+    #print("all null points: ", cf_list)
+    cf_column.append(cf_list)
+    res_sep_min_s=np.zeros((3, 4))
+    #print(res_f)
+    #print(type(res_f))
+    for i in range(len(res_f)):
+        for j in range(len(res_f[i])):
+            #print("j: ", j)
+            if len(cf_list[i][j]) != 0:
+                res_sep_min_s[i][j]=angular_separation_deg(res_maxpoint, cf_list[i][j])
+    max_sep_column.append(res_sep_min_s.tolist())
+    print("RES.OUT")
+    #print("90% probability area: ", get_area("./O4A_fits/", current_superevent_array, area_percent=90))
+    superevent_array_map(current_superevent_array, current_superevent_array[0], file_location="./O4a_fits/") 
+results_d = {'Event name':namescolumn, 'Event time':timescolumn, 'Detectors':detectorscolumn, 'Probabilities':p_column, 'Significant figures':sf_column, 'Flags':f_column, 'All null points':cf_column, 'Max point':maxpoint_column, 'Separations from maximum point':max_sep_column}
+results_df=pd.DataFrame(data=results_d)
+print(results_df)
+results_df.to_excel("Sep_events_1_CPSP.xlsx") 
---
+++ TestPercentAreaPandas.py
@@ -0,0 +1,51 @@
+import numpy as np
+import math
+from CoordsProbabilityCFolder.PercentArea import *
+from CoordsProbabilityCFolder.O4aSuperevents import *
+from CoordsProbabilityCFolder.DetectorMinima import *
+import pandas as pd
+
+iterator0 = 0
+nameslist = []
+area_90_list = []
+area_50_list = []
+sep_90_list = []
+sep_50_list = []
+print("len(O4a_Aug_events_1):", len(O4a_Aug_events_1))
+for i in range(len(O4a_Aug_events_1)):
+    print("iterator:", iterator0)
+    current_superevent_array=O4a_Aug_events_1[i]
+    print("Event name: ", current_superevent_array[0])
+    nameslist.append(current_superevent_array[0])
+    print("Event time: ", current_superevent_array[2])
+    file_location="Downloads/CoordsProbabilityAllEvents/O4a_fits/"
+    superevent_fits=file_location+current_superevent_array[0]+"_bayestar_fits_gz_"+str(current_superevent_array[1])+".fits"
+    #print(superevent_fits)
+    #print("90% area: ", get_area(file_location, current_superevent_array, 90))
+    area_90_list.append(get_area(file_location, current_superevent_array, 90))
+    area_50_list.append(get_area(file_location, current_superevent_array, 50))
+    #print("50% area: ", get_area(file_location, current_superevent_array, 50))
+    #print("50% area distance from coordinate zero: ", get_area_distance(file_location, current_superevent_array, [0,0], 50, order=8))    #print("90% area distance from coordinate zero: ", get_area_distance(file_location, current_superevent_array, [0,0], 90, order=8))
+    cf_list=get_all_null_coords(current_superevent_array[2], which_detectors(current_superevent_array[3]))
+    #print("cf_list:", cf_list)
+    res_sep_min_50=np.zeros((3, 4))
+    res_sep_min_90=np.zeros((3, 4))
+    for i in range(3):
+        for j in range(4):
+            if len(cf_list[i]) !=0:
+                if len(cf_list[i][j]) != 0:
+                    current_point=cf_list[i][j]
+                    res_sep_min_50[i][j]=get_area_distance(file_location, current_superevent_array, current_point, 50, order=8)
+                    res_sep_min_90[i][j]=get_area_distance(file_location, current_superevent_array, current_point, 90, order=8)
+    #print("angular separations from 90% area: ", res_sep_min_90.tolist())
+    sep_90_list.append(res_sep_min_90.tolist())
+    sep_50_list.append(res_sep_min_50.tolist())
+    #print("angular separations from 50% area: ", res_sep_min_50.tolist())
+    #print("Event name: ", current_superevent_array[0])
+    #print("RES.OUT")
+    iterator0 = iterator0+1
+results_d = {'Event name':nameslist, '90% area':area_90_list, '50% area':area_50_list, 'distances from 90% area':sep_90_list, 'distances from 50% area':sep_50_list}
+results_df = pd.DataFrame(data=results_d)
+print(results_df)
+results_df.to_excel("Aug_events_1_AD.xlsx") 
+        
